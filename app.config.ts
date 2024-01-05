2a15d606c8b594ec1db7d7a00153502da43c77f4
Andrii Zinko <andrii.zinko@sysgears.com>
2024-01-05 13:10:20 +0200

    Replace app.json with app.config.ts.

---
+++ app.config.ts
@@ -0,0 +1,24 @@
+import { ConfigContext, ExpoConfig } from 'expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'Pammagram',
+  slug: 'pammagram',
+  description: 'Pammagram messenger',
+  version: '1.0.0',
+  orientation: 'portrait',
+  userInterfaceStyle: 'light',
+  splash: {
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      backgroundColor: '#ffffff',
+    },
+  },
+});

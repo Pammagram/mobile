c8a75cbe67321c7d56bb1408a69a73f06b21a8ad
Andrii Zinko <andrii.zinko@sysgears.com>
2024-01-09 20:22:09 +0200

    Add custom apollo hooks.

---
+++ src/shared/apollo/hooks/index.ts
@@ -0,0 +1,6 @@
+export * from './useCustomQuery';
+export * from './useCustomLazyQuery';
+export * from './useCustomSubscription';
+export * from './useCustomMutation';
+export * from './constants';
+export * from './types';
---
+++ src/shared/apollo/hooks/useCustomQuery/index.ts
@@ -0,0 +1,68 @@
+import {
+  DocumentNode,
+  OperationVariables,
+  QueryHookOptions,
+  TypedDocumentNode,
+  useQuery,
+} from '@apollo/client';
+import merge from 'lodash/merge';
+import { useCallback } from 'react';
+
+import { PrefixedQueryResult, StrictPrefixedQueryResult } from './types';
+
+import { DEFAULT_RESPONSE_NAME } from '../constants';
+
+import { GraphQlResponse } from '$shared/apollo/hooks/types';
+import { getGraphqlRequestKey } from '$shared/apollo/hooks/utils';
+
+export * from './types';
+export const useCustomQuery = <
+  Prefix extends string,
+  Data,
+  Variables extends OperationVariables,
+  Strict extends boolean = false,
+>(
+  prefix: Prefix,
+  document: DocumentNode | TypedDocumentNode<GraphQlResponse<Data>, Variables>,
+  options?: QueryHookOptions<GraphQlResponse<Data>, Variables>,
+): Strict extends true
+  ? StrictPrefixedQueryResult<Prefix, Data, Variables>
+  : PrefixedQueryResult<Prefix, Data, Variables> => {
+  const { data, ...restQueryResult } = useQuery<
+    GraphQlResponse<Data>,
+    Variables
+  >(document, options);
+
+  const { fetchMore } = restQueryResult;
+  const customFetchMore = useCallback(
+    async (fetchMoreVariables: Variables): Promise<Data | undefined> => {
+      const variables = merge(
+        options?.variables,
+        fetchMoreVariables,
+      ) satisfies Record<string, string>;
+
+      try {
+        const result = await fetchMore({
+          variables,
+        });
+
+        return result?.data[DEFAULT_RESPONSE_NAME];
+      } catch (error) {
+        return undefined;
+      }
+    },
+    [fetchMore, options?.variables],
+  );
+
+  const queryResult = {
+    data: data?.[DEFAULT_RESPONSE_NAME], // * Response must contain field {DEFAULT_RESPONSE_NAME}
+    ...restQueryResult,
+    fetchMore: customFetchMore,
+  };
+
+  return {
+    [getGraphqlRequestKey(prefix)]: queryResult,
+  } as Strict extends true
+    ? StrictPrefixedQueryResult<Prefix, Data, Variables>
+    : PrefixedQueryResult<Prefix, Data, Variables>;
+};

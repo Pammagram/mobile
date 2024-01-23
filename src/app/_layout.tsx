78f5cbe948bf1d6d3d94aafbcb0a0c12d91b89fd
Andrii Zinko <andrii.zinko@sysgears.com>
2024-01-23 18:05:24 +0200

    Update app pre provider.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,21 @@
-import { Slot } from 'expo-router';
+import { Slot, SplashScreen } from 'expo-router';
 import { FC } from 'react';
 
+import { usePreloadedAssets } from '$core/assets';
 import { combineProviders } from '$core/providers';
+
+SplashScreen.preventAutoHideAsync();
 
 const manager = combineProviders();
 const MasterProvider = manager.master();
 
 const PreProviderApp: FC = () => {
+  const { isAssetsLoaded } = usePreloadedAssets();
+
+  if (!isAssetsLoaded) {
+    return null;
+  }
+
   return (
     <MasterProvider>
       <Slot />

7cd4fdee9d925bcaf47c7224320227f34679ce7f
Andrii Zinko <andrii.zinko@sysgears.com>
2024-01-31 15:10:49 +0200

    Add currentUser feature.

---
+++ src/features/currentUser/graphql/query.ts
@@ -0,0 +1,13 @@
+import { createMe } from '$entities';
+import { InferSelection } from '$shared';
+
+export const PREFIX = 'me';
+
+export const QUERY = createMe({
+  id: true,
+  lastActiveInMs: true,
+  phoneNumber: true,
+  username: true,
+});
+
+export type Data = InferSelection<typeof QUERY>;
---
+++ src/features/currentUser/hooks/useCurrentUser.ts
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+
+import { useMe } from '../graphql';
+
+import { userVar } from '$entities';
+import { UserDto } from '$shared';
+
+type ReturnType = {
+  isLoading: boolean;
+  user: UserDto | null;
+};
+
+export const useCurrentUser = (): ReturnType => {
+  const {
+    getMe: { data, loading: isLoading },
+  } = useMe({
+    fetchPolicy: 'network-only',
+    onError: () => null,
+  });
+
+  useEffect(() => {
+    userVar(data ?? null);
+  }, [data?.id]);
+
+  return {
+    user: data ?? null,
+    isLoading,
+  };
+};

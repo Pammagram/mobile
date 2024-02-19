c93f6e4f5459fa90fcc3cc6d141c9970b3f8dec8
Maksym Pryhoda <maksym.pryhoda@sysgears.com>
2024-02-19 18:19:40 +0200

    Minor fixes.

--- src/features/logout/graphql/index.ts
+++ src/features/logout/graphql/index.ts
@@ -1,8 +1,14 @@
+import { OperationVariables } from '@apollo/client';
+
 import { Data, MUTATION, PREFIX } from './mutation';
 
 import { useCustomMutation, UseMutationWrapper } from '$shared';
 
-export type UseLogout = UseMutationWrapper<typeof PREFIX, Data, {}>;
+export type UseLogout = UseMutationWrapper<
+  typeof PREFIX,
+  Data,
+  OperationVariables
+>;
 
 export const useLogout: UseLogout = (...args) =>
   useCustomMutation(PREFIX, MUTATION, ...args);
